07684fab21cda4a73cf974e19eb385be8c3b0b2a
JorPan <jbpanasewicz@gmail.com>
2020-11-02 17:05:19 -0700

    commit tree 5f7037ba4af71a975a5fc05e1a1aae30e9f37b5b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+require "pry"
+
+class School
+
+    attr_reader :name
+
+    @@all = []
+
+    def initialize name
+        @name = name
+        @@all << self
+    end
+
+roster = {}
+
+    def roster
+        {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] = []
+        roster[grade].push(name)
+    end
+
+
+end
